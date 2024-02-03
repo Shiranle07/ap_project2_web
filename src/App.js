e081cd23491fb3bb3ffe8a877e03894fa79e678c
Shiran Levi <shiranle07@gmail.com>
2024-02-03 21:52:05 +0200

    dark mode feature

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import LoginPage from './loginPage/LoginPage';
 import './App.css';
 import LoginLogo from './loginPage/loginLogo/LoginLogo';
 
+import React from 'react';
+import DarkMode from './darkMode/DarkMode';
+
 function App() {
   return (
     <div>
         <LoginPage/>
         <LoginLogo/>
+        <DarkMode />
 
     </div>
   );
--- src/loginPage/LoginPage.js
+++ src/loginPage/LoginPage.js
@@ -2,27 +2,27 @@ import './LoginPage.css';
 
 function LoginPage(){
     return(
-        <div>
-            <div className="form-floating col-4 bg-white justify-content-center align-items-center" >
+        <div className='form-box col-4 justify-content-center align-items-center'>
+            <form className="needs-validation" >
                 <div className="row justify-content-center align-items-center">
                     <div className="col-11">
-                        <label for="validationDefaultUsername" class="form-label"></label>
-                        <input type="text" class="form-control" id="validationDefaultUsername" placeholder ="Email" aria-describedby="inputGroupPrepend2" required></input>
+                        <label for="validationDefaultUsername" className="form-label"></label>
+                        <input type="text" className="form-control" id="validationDefaultUsername" placeholder ="Email" aria-describedby="inputGroupPrepend2" required></input>
                     </div>
                 </div>
 
                 <div className="row justify-content-center align-items-center mb-3">
                     <div className="col-11">
-                        <label for="inputPassword4" class="form-label"></label>
+                        <label for="inputPassword4" className="form-label"></label>
                         <div className='input-group'>
-                            <input type="password" class="form-control" placeholder="Password" id="validationDefault03" aria-describedby="inputGroupPrepend2" required></input>
+                            <input type="password" className="form-control" placeholder="Password" id="validationDefault03" aria-describedby="inputGroupPrepend2" required></input>
                         </div>
                     </div>
                 </div>
 
                 <div className="row justify-content-center align-items-center pt-2 mb-2">
                     <div className="d-grid gap-2 col-11 mx-auto">
-                        <button type="submit" class="btn btn-primary btn-login">Log In</button>
+                        <button type="submit" className="btn btn-primary btn-login">Log In</button>
                     </div>
                 </div>
 
@@ -30,19 +30,19 @@ function LoginPage(){
                     <div className="text-center">
                         <a className="link-offset-2 link-offset-1-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover" href="#"> Forgot password?</a>
                     </div>
+                </div>
+            </form> 
+
+            <div className="new-account row justify-content-center align-items-center pt-2">
+                <div className="line"></div>
             </div>
 
-
-                <div className="row justify-content-center align-items-center pt-2">
-                    <div className="line"></div>
+            <div className="new-account row justify-content-center align-items-center pt-2">
+                <div className="d-grid gap-2 col-6 mx-auto">
+                    <button type="submit" className="btn btn-success btn-sign">Create new account</button>
                 </div>
-
-                <div className="row justify-content-center align-items-center pt-2">
-                    <div className="d-grid gap-2 col-6 mx-auto">
-                        <button type="submit" class="btn btn-success btn-sign">Create new account</button>
-                    </div>
-                </div>
-            </div>           
+            </div>
+          
         </div>
     );
 }
